861b854e3849db9406201256b98dd407d4c4b5de
bswat01 <beswat14@gmail.com>
2018-12-04 09:40:30 -0700

    Added Employee and Location Models

---
+++ Intex/DAL/ContextIndex.cs
@@ -0,0 +1,20 @@
+﻿using Intex.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Intex.DAL
+{
+    public class ContextIntex : DbContext
+    {
+        public ContextIntex()
+            : base("ContextIntex")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Location> Locations { get; set; }
+    }  
+}
---
+++ Intex/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Intex.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public int LocationID { get; set; }
+        public virtual Location Location { get; set; }
+    }
+}
