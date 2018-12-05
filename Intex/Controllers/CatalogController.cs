0964ffaf6f5c368759d00c574394cd280c2419da
bswat01 <beswat14@gmail.com>
2018-12-05 13:29:57 -0700

    ActionLinks work

--- Intex/Controllers/CatalogController.cs
+++ Intex/Controllers/CatalogController.cs
@@ -13,5 +13,10 @@ namespace Intex.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("Login", "Home");
+        }
     }
 }
